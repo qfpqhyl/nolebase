f7250ff9729d4effdde898929c459c36908d70aa
tianbosong <tianbosong2674@gmail.com>
2025-07-22 10:32:11 +0800

    init

--- metadata/index.ts
+++ metadata/index.ts
@@ -9,7 +9,7 @@ export const include = ['笔记', '生活']
 /** Repo */
 export const githubRepoLink = 'https://github.com/qfpqhyl'
 /** Discord */
-// export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+export const discordLink = 'https://discord.gg/XuNFDcDZGj'
 
 /** 无协议前缀域名 */
 export const plainTargetDomain = 'blog.qfpqhyl.cloudns.org/'
