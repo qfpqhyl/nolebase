37f055a8b0278ef478683538c0dfe15a1981738b
tianbosong <tianbosong2674@gmail.com>
2025-07-22 10:30:42 +0800

    init

--- index.md
+++ index.md
@@ -2,11 +2,11 @@
 layout: home
 sidebar: false
 
-title: Nólëbase
+title: 秋风飘起黄叶落
 titleTemplate: 记录回忆，知识和畅想的地方
 
 hero:
-  name: g~Nj$3J2^
+  name: 秋风飘起黄叶落
   text: 记录回忆，知识和畅想的地方
   tagline: 以 Nólëbase 为名，读作 nole-base，取自意为「知识」的昆雅语 nólë 和意为「基础」的英文 base，即「知识库」
   image:
@@ -16,12 +16,12 @@ hero:
     - theme: brand
       text: 开始阅读
       link: /笔记/index
-    - theme: alt
-      text: 加入 Discord 服务器
-      link: https://discord.gg/XuNFDcDZGj
+    # - theme: alt
+    #   text: 加入 Discord 服务器
+    #   link: https://discord.gg/XuNFDcDZGj
     - theme: alt
       text: GitHub 上浏览
-      link: https://github.com/nolebase/nolebase
+      link: https://github.com/qfpqhyl
 
 features:
   - title: 多样的主题和内容
--- metadata/index.ts
+++ metadata/index.ts
@@ -1,17 +1,17 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = '秋风飘起黄叶落'
+export const siteShortName = '秋风飘起黄叶落'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://github.com/qfpqhyl'
 /** Discord */
-export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+// export const discordLink = 'https://discord.gg/XuNFDcDZGj'
 
 /** 无协议前缀域名 */
-export const plainTargetDomain = 'nolebase.ayaka.io'
+export const plainTargetDomain = 'blog.qfpqhyl.cloudns.org/'
 /** 完整域名 */
 export const targetDomain = `https://${plainTargetDomain}`
